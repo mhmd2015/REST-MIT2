428449a272d5f71b4c6246b1a927a64365c4305d
mhmd2025 <140452480+mhmd2025@users.noreply.github.com>
2023-07-26 17:20:37 -0500

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
   return (
     <>
       <div>MHMD</div>
-      <div>{process.env.HOTS_WEB_URI}</div>
+      <div>{process.env.HOST_WEB_URI}</div>
       <nav className={styles.navbar}>
         <Link href="/">
           <div className={styles.navbar_branding}>
