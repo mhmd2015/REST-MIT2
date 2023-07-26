f9e34522df95ffc4a88debd740c968ff64461417
mhmd2025 <140452480+mhmd2025@users.noreply.github.com>
2023-07-26 14:44:44 -0500

    Update connectDb.js

--- config/connectDb.js
+++ config/connectDb.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
+console.log(process.env.DB_URI)
+
+process.env.DB_URI = "mongodb+srv://mhmdmcsd:Passw0rd1@cluster0.tuflupv.mongodb.net/?retryWrites=true&w=majority"
 const connectDb = async () => {
   try {
     await mongoose.connect(process.env.DB_URI, {
