794309afdb0a8c26fab2e376011d00daa92cedb6
mhmd2025 <140452480+mhmd2025@users.noreply.github.com>
2023-07-28 07:41:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
 
     domains: ["restaurant-images-hadeel.s3.us-east-2.amazonaws.com"],
   },
   //output: 'export',
+  useFileSystemPublicRoutes: false,
   output:"standalone"
 };
 
